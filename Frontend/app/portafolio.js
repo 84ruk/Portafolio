3d71d1a22e94c9d62d221249bdea75b8f02a2459
baruk <baruk066@gmail.com>
2023-11-20 13:41:13 -0600

    fix errors

--- Frontend/app/portafolio.js
+++ Frontend/app/portafolio.js
@@ -34,17 +34,15 @@ export default function Portafolio() {
 
         <div className="slider-container w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 mx-auto shadow-xl max-w-xl">
   <div className="slider-card">
-    <div className="rounded-t-lg">
-      <Image src={Pokedex} alt='Imagen proyecto' />
-    </div>
-    <div className="p-4">
-      <h2 className="text-xl sm:text-2xl font-semibold text-gray-800">Pokedex</h2>
-      <p className="text-gray-600 mb-4">
-        Pokedex es una aplicación web que muestra información sobre Pokémon. La aplicación obtiene datos de una API y permite filtrar por tipo, buscar por nombre y utilizar desplazamiento infinito para una experiencia fluida.
-      </p>
-      <div className="mb-4">
-        <h3 className="text-lg font-semibold text-gray-800">Tecnologías Utilizadas</h3>
-        <ul className="list-disc pl-6 text-gray-600">
+  <div className="rounded-t-lg"><Image src={Playcar} alt='Imagen proyecto'/></div>
+      <div className="p-5">
+        <h2 className="text-2xl font-semibold text-gray-800">Play Car - App estilo Airbnb</h2>
+        <p className="text-gray-600 mb-4">
+        Este proyecto es una plataforma para agencias de viajes que permite a los clientes registrarse, reservar servicios y explorar el catálogo. Incluye un panel de administración para la gestión de productos y la agenda. Utiliza Server-Side Rendering (SSR) con Next.js para una carga rápida y se ha optimizado para SEO.
+        </p>
+        <div className="mb-4">
+          <h3 className="text-lg font-semibold text-gray-800">Tecnologías Utilizadas</h3>
+          <ul className="list-disc pl-6 text-gray-600">
             <li>HTML5</li>
             <li>CSS3</li>
             <li>JavaScript</li>
@@ -54,25 +52,27 @@ export default function Portafolio() {
             <li>MongoDB</li>
           </ul>
         </div>
-        <div className="flex flex-col items-center md:flex-row md:justify-between ">
-        <Link
-          href="https://github.com/84ruk/PokeAPI"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mt-2 px-4 py-2 bg-blue-600 text-white font-bold rounded-md shadow-md hover:bg-blue-500 transition-colors duration-300 hover:shadow-none"
-        >
-          Repositorio
-        </Link>
-        <Link
-          href="https://precious-zabaione-c7a52d.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mt-2 px-4 py-2 text-black font-bold rounded-md shadow-md transition-colors duration-300 hover:shadow-none"
-        >
-          Ver Proyecto
-        </Link>
+        <div className="flex justify-between ">
+      <Link
+            href="https://github.com/84ruk/PlayCar"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="px-4 py-2 bg-blue-600 text-white font-bold rounded-md shadow-md hover:bg-blue-500 transition-colors duration-300 hover:shadow-none"
+          >
+            Repositorio
+          </Link>
+          <Link
+            href="https://play-car.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="px-4 py-2 text-black font-bold rounded-md shadow-md transition-colors duration-300 hover:shadow-none"
+          >
+            Ver Proyecto
+          </Link>
       </div>
       </div>
+
+
 
     </div>
 
@@ -138,10 +138,9 @@ export default function Portafolio() {
       <div className="mb-4">
         <h3 className="text-lg font-semibold text-gray-800">Tecnologías Utilizadas</h3>
         <ul className="list-disc pl-6 text-gray-600">
-          <li>HTML5</li>
-          <li>CSS3</li>
-          <li>JavaScript</li>
-          <li>React</li>
+          <li>Python</li>
+          <li>Matplotlib</li>
+          <li>NumPy</li>
         </ul>
       </div>
       <div className="flex flex-col items-center md:flex-row md:justify-between ">
